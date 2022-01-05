7acb629a06e216bea0e2a4171add296d397ec637
RealSoerensen <patricklykke@live.dk>
2022-01-05 01:00:04 +0100

    Updated for replit

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
-app = Flask(__name__, static_folder="templates/assets", template_folder="templates")
+
+app = Flask(__name__,
+    static_folder="templates/assets",
+    template_folder="templates")
+
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
