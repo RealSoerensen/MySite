57eabe6ba18204ff025ae75cbf87bfd93806d798
RealSoerensen <patricklykke@live.dk>
2022-02-04 04:43:41 +0100

    Added date and time for update

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template
 from pathlib import Path
 from api_refresh import Stats
-import threading
+from threading import Thread
+from time import sleep
 
 path = Path(__file__).parent.resolve()
 app = Flask(__name__, 
-    static_folder=str(path) + "/assets", 
-    template_folder=path)
+  static_folder=str(path) + "/assets", 
+  template_folder=path)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+  return render_template('index.html')
 
 # Refresh stats every 24 hours
 def refresh():
-    threading.Timer(60*60*24, refresh).start()
-    stat = Stats()
-    stat.update()
+  stat = Stats()
+  stat.update()
+  print("Stats updated")
+  sleep(60*60*24)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-    refresh()
+  Thread(target=refresh).start()
+  app.run(host="0.0.0.0")
